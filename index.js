09d01b16e22643e97269efd79f9a8a122a32fef3
Satish Yadav <satish.yadav@nagarro.com>
2022-09-23 17:25:23 +0530

    Bind Actions Creators method has been used to bind actions. Readme file updated

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const redux = require('redux');
 const createStore = redux.legacy_createStore;
 const middleware = redux.applyMiddleware;
+const bindActions = redux.bindActionCreators;
 const { createLogger } = require('redux-logger');
 const produce = require('immer').produce;
 
@@ -87,21 +88,27 @@ const store = createStore(rootReducer, middleware(logger));
 // 	console.log('Updated Store', store.getState()),
 // );
 
+// Bind actions
+const actions = bindActions(
+	{ makeACake, sellACake, makeAIceCream, sellAIceCream },
+	store.dispatch,
+);
+
 // Perform Updates in Cake
-store.dispatch(makeACake());
-store.dispatch(makeACake());
-store.dispatch(makeACake());
-store.dispatch(sellACake());
-store.dispatch(sellACake());
-store.dispatch(sellACake());
+actions.makeACake();
+actions.makeACake();
+actions.makeACake();
+actions.sellACake();
+actions.sellACake();
+actions.sellACake();
 
 // Perform Updates in Ice-Cream
-store.dispatch(makeAIceCream());
-store.dispatch(makeAIceCream());
-store.dispatch(makeAIceCream());
-store.dispatch(sellAIceCream());
-store.dispatch(sellAIceCream());
-store.dispatch(sellAIceCream());
+actions.makeAIceCream();
+actions.makeAIceCream();
+actions.makeAIceCream();
+actions.sellAIceCream();
+actions.sellAIceCream();
+actions.sellAIceCream();
 
 // Unsubscribe Store
 // unSubscribe();
