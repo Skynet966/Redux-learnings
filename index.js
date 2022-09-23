48e11e442af4e9a114be9f2f428d2c706cd1497b
Satish Yadav <satish.yadav@nagarro.com>
2022-09-23 14:59:16 +0530

    Redux logger middleware applied using applyMiddleware function of redux

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const redux = require('redux');
 const createStore = redux.legacy_createStore;
+const middleware = redux.applyMiddleware;
+const { createLogger } = require('redux-logger');
 
 // Cake Types
 const MAKE_CAKE = 'MAKE_CAKE';
@@ -11,27 +13,27 @@ const SELL_ICE_CREAM = 'SELL_ICE_CREAM';
 
 // Cake Action
 const makeACake = () => ({
-	type: MAKE_CAKE
+	type: MAKE_CAKE,
 });
 const sellACake = () => ({
-	type: SELL_CAKE
+	type: SELL_CAKE,
 });
 
 // Ice-cream Action
 const makeAIceCream = () => ({
-	type: MAKE_ICE_CREAM
+	type: MAKE_ICE_CREAM,
 });
 const sellAIceCream = () => ({
-	type: SELL_ICE_CREAM
+	type: SELL_ICE_CREAM,
 });
 
 // Initial Cake State
 const initialCakeState = {
-	noOfCake: 10
+	noOfCake: 10,
 };
 // Initial Cake State
 const initialIceCreamState = {
-	noOfIceCream: 10
+	noOfIceCream: 10,
 };
 
 // Cake Reducer
@@ -40,12 +42,12 @@ const cakeReducer = (state = initialCakeState, { type, payload }) => {
 		case MAKE_CAKE:
 			return {
 				...state,
-				noOfCake: state.noOfCake + 1
+				noOfCake: state.noOfCake + 1,
 			};
 		case SELL_CAKE:
 			return {
 				...state,
-				noOfCake: state.noOfCake - 1
+				noOfCake: state.noOfCake - 1,
 			};
 		default:
 			return state;
@@ -58,12 +60,12 @@ const iceCreamReducer = (state = initialIceCreamState, { type, payload }) => {
 		case MAKE_ICE_CREAM:
 			return {
 				...state,
-				noOfIceCream: state.noOfIceCream + 1
+				noOfIceCream: state.noOfIceCream + 1,
 			};
 		case SELL_ICE_CREAM:
 			return {
 				...state,
-				noOfIceCream: state.noOfIceCream - 1
+				noOfIceCream: state.noOfIceCream - 1,
 			};
 		default:
 			return state;
@@ -73,17 +75,18 @@ const iceCreamReducer = (state = initialIceCreamState, { type, payload }) => {
 // Combine Cake and Ice-cream Reducers
 const rootReducer = redux.combineReducers({
 	cake: cakeReducer,
-	iceCream: iceCreamReducer
+	iceCream: iceCreamReducer,
 });
 
+const logger = createLogger();
 // Create store;
-const store = createStore(rootReducer);
-console.log('Initial Store', store.getState());
+const store = createStore(rootReducer, middleware(logger));
+// console.log('Initial Store', store.getState());
 
 // Subscribe to store
-const unSubscribe = store.subscribe(() =>
-	console.log('Updated Store', store.getState())
-);
+// const unSubscribe = store.subscribe(() =>
+// 	console.log('Updated Store', store.getState()),
+// );
 
 // Perform Updates in Cake
 store.dispatch(makeACake());
@@ -102,4 +105,4 @@ store.dispatch(sellAIceCream());
 store.dispatch(sellAIceCream());
 
 // Unsubscribe Store
-unSubscribe();
+// unSubscribe();
